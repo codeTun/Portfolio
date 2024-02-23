a104b54561a224cf247573e5c0567a0e6b9d5a1c
codeTun <iheblazhary@gmail.com>
2024-02-23 23:51:28 +0100

    -Test background image !

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,7 @@ import Contact from "./pages/contact/Contact";
 import PageNotFound from "./pages/404/PageNotFound";
 import { Analytics } from "@vercel/analytics/react"
 
-/**
- * Instructions for Customizing the Portfolio
- * ------------------------------------------
- * 1. Adding Your Own Projects:
- *    - Navigate to the "_data" folder and modify the "projects.json" file to include your projects.
- *
- * 2. Replacing Project Images:
- *    - Access the "public/projectImages" directory to replace the my project images with your own.
- *
- * 3. Handling Form Submissions:
- *    - To receive form submissions, obtain an API Key. Refer to the Form component for detailed instructions.
- *
- * For Assistance or Questions:
- * -----------------------------
- * If you require assistance or have questions, don't hesitate to reach out via LinkedIn or email.
- *
- * Support and Star:
- * ------------------
- * Enjoying this project? Please consider giving it a star (🌟).
- * I'm committed to ongoing updates and feature additions.
- * Your suggestions and feedback are invaluable and highly encouraged!
- */
+
 
 function App() {
   // Personal details for the user
