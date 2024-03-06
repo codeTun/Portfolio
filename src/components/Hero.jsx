285344bfb20b613b7a827adabb5e5ddb7c8a593b
codeTun <iheblazhary@gmail.com>
2024-03-07 00:56:23 +0100

    -Modified some features  📌

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,82 +19,89 @@ const Hero = ({ name }) => {
   };
 
   const styles = {
-    landingImage: {
-      position: "absolute",
-      margin: "auto",
-      opacity: "0.2",
-      width: "500px", // Set a fixed width
-
-      maxWidth: windowWidth <= 768 ? "500px" : "800px",
-      height: windowWidth <= 768 ? "auto" : undefined, // Set a fixed height
-      borderRadius: "50%", // Make the image a circle
-      objectFit: "cover",
+    parentContainer: {
+      display: "flex",
+      flexDirection: windowWidth <= 768 ? "column-reverse" : "row",
+      justifyContent: "center",
+      alignItems: "center",
     },
-
+    landingImage: {
+      position: windowWidth > 768 ? "absolute" : "relative",
+      margin: "0",
+      opacity: "0.2",
+      width: windowWidth <= 768 ? "100%" : "30%", // Adjust the width based on the window width
+      height: "auto",
+      borderRadius: "50%",
+      objectFit: "cover",
+      top: windowWidth > 768 ? "55%" : "55%",
+      left: windowWidth > 768 ? "68%" : "auto",
+      transform: windowWidth > 768 ? "translateY(-50%)" : "none",
+    },
     textContainer: {
-      
+      display: "flex",
       flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
       letterSpacing: "1px",
       textAlign: "center",
       zIndex: "1",
       color: "var(--text-color)",
       textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
-      padding: "23px",
-      marginTop: "-70px",
-      marginRight: "50px",
+      padding: "20px",
+      fontSize: windowWidth <= 768 ? "1.5em" : "1.5em", // Adjust the text size based on the window width
     },
-
     name: {
       color: "var(--hl-color)",
       fontWeight: "700",
-      marginTop: "-100px",
-      paddingBottom: "30px",
-      
+      paddingBottom: "20px",
+      fontSize: windowWidth <= 768 ? "1.8em" : "2.5em", // Adjust the text size based on the window width
     },
   };
 
   return (
     <>
-      <div className="textContainer" style={styles.textContainer}>
-        <motion.h1
-          className="name"
-          style={styles.name}
-          initial={{ scale: 0.5, opacity: 0 }}
-          animate={controls}
-          transition={{ delay: 0, duration: 0.5, type: "spring" }}
-        >
-          {name}
-        </motion.h1>
-        <motion.div
-          className="description"
-          initial={{ y: -100, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.4, type: "spring" }}
-        >
-          <Typewriter
+      <div style={styles.parentContainer}>
+        <div className="textContainer" style={styles.textContainer}>
+          <motion.h1
+            className="name"
+            style={styles.name}
+            initial={{ scale: 0.5, opacity: 0 }}
+            animate={controls}
+            transition={{ delay: 0, duration: 0.5, type: "spring" }}
+          >
+            {name}
+          </motion.h1>
+          <motion.div
             className="description"
-            options={{
-              cursor: "",
-            }}
-            onInit={(typewriter) => {
-              typewriter
-                .changeDelay(50)
-                .typeString("Software Engineer")
-                .start();
-            }}
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.4, type: "spring" }}
+          >
+            <Typewriter
+              className="description"
+              options={{
+                cursor: "",
+              }}
+              onInit={(typewriter) => {
+                typewriter
+                  .changeDelay(50)
+                  .typeString("Software Engineer")
+                  .start();
+              }}
+            />
+          </motion.div>
+        </div>
+        <div className="image-container">
+          <motion.img
+            className="landingImage"
+            initial={{ opacity: 0.5 }}
+            animate={{ opacity: 0.9 }}
+            transition={{ delay: 1, duration: 1.5, ease: "easeInOut" }}
+            style={styles.landingImage}
+            src={landingImage}
+            alt="Elazheri Iheb"
           />
-        </motion.div>
-      </div>
-      <div className="image-container">
-        <motion.img
-          className="landingImage"
-          initial={{ opacity: 0.5 }}
-          animate={{ opacity: 0.9 }}
-          transition={{ delay: 1, duration: 1.5, ease: "easeInOut" }}
-          style={styles.landingImage}
-          src={landingImage}
-          alt="Elazheri Iheb"
-        />
+        </div>
       </div>
       <motion.div
         initial={{ opacity: 0 }}
