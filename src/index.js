a7501f7cd0d51222ed7aa45ae62cfb95411f1fd1
codeTun <iheblazhary@gmail.com>
2024-08-09 23:43:28 +0100

    🎖️  Modificed the languages | Added i18next to the index file

--- src/index.js
+++ src/index.js
@@ -8,18 +8,40 @@ import "./pages/resume/resume.css";
 import "./pages/contact/contact.css";
 import "./pages/404/pageNotFound.css";
 import { Analytics } from "@vercel/analytics/react";
-import 'flowbite/dist/flowbite.css';
+import "flowbite/dist/flowbite.css";
 
 import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
+import i18next from "i18next";
+import global_en from "./locales/en/global.json";
+import global_fr from "./locales/fr/global.json";
+import global_ar from "./locales/ar/global.json";
+
+i18next.init({
+  interpolate: { escapeValue: false },
+  lng: "en",
+  resources: {
+    en: {
+      global: global_en,
+    },
+    fr: {
+      global: global_fr,
+    },
+    ar: {
+      global: global_ar,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Analytics />
-    </BrowserRouter>
+    <i18nextProvider i18n={i18next}>
+      <BrowserRouter>
+        <App />
+        <Analytics />
+      </BrowserRouter>
+    </i18nextProvider>
   </React.StrictMode>
 );
